df6fc13b4cb6bed1ef1b4e48b2179437e509f12b
mohideen-openwave <mohideen.openwave@gmail.com>
2013-10-01 16:45:34 +0530

    Beta 2.0
    Added admin interface to display User's profile.

---
+++ oppia/profile/admin.py
@@ -0,0 +1,14 @@
+# oppia/profile/admin.py
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+
+from oppia.profile.models import CustomUser
+
+
+class ProfileAdmin(admin.ModelAdmin):   
+    list_display = ('phoneno', 'professional', 'town', 'city', 'state', 'country', 'worktype', 'currentlyworking', 'stafftype', 'familyplaning', 'providedit', 'nurhitraining', 'education', 'religion', 'sex', 'age') #user.email creates the error - tried some variations here, but no luck.
+
+admin.site.register(CustomUser, ProfileAdmin)
+admin.autodiscover()
+
